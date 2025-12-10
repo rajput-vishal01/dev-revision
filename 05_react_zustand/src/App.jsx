e33a0276cf220f299ff3879d45eafe8fb9dc7f8c
Vishal Rajput <vishalrajput.7vr@gmail.com>
2025-12-10 16:20:53 +0530

    added:05_react_zustand

---
+++ 05_react_zustand/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+import Posts from "./components/Posts";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Zustand</h1>
+      <Counter />
+      <Posts />
+    </>
+  );
+}
+
+export default App;
